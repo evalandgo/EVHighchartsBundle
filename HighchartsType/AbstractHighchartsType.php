32d10481978721938a6a9b91698deee318f7398f
nathan <nathan.bourdette@gmail.com>
2015-08-13 10:10:58 +0200

    Remove container to AbstractType

--- HighchartsType/AbstractHighchartsType.php
+++ HighchartsType/AbstractHighchartsType.php
@@ -4,17 +4,14 @@ namespace EV\HighchartsBundle\HighchartsType;
 
 use EV\HighchartsBundle\Builder\HighchartsBuilder;
 
-use Symfony\Component\DependencyInjection\ContainerAware;
-
 /**
  * Description of AbstractHighchartsType
  *
  * @author Nathan
  */
-abstract class AbstractHighchartsType extends ContainerAware{
+abstract class AbstractHighchartsType {
 
     abstract protected function buildHighcharts(HighchartsBuilder $highchartsBuilder);
-
 }
 
 ?>
